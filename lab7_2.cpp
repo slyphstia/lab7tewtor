53bc5494ed4dfedb1246e688a5c744035a941318
slyphstia <apiwit_boonyarit@cmu.ac.th>
2022-12-13 17:17:02 +0700

    complete

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -4,11 +4,29 @@ using namespace std;
 
 int main(){
 	
-	int i = 0, P[5000], k, result;
+	int i = 0, P[5000], k, result = 0;
 	while(i < 5000){
 		P[i] = 2*i+1;
 		i++;
 	}
-	
+	cout << "please input k: ";
+	cin >> k;
+	if(k>0){
+		while(i<5000){
+			if(P[i]%k==0){
+				result=result-P[i];
+				i=i+1;
+			}
+			else{
+				result=result+P[i];
+				i=i+1;
+			}
+		}
+		cout <<"result = " << result ;
+
+	}
+    else{
+		cout << "invalid input!!!";
+	}
 	return 0;
 }
